aff42cb68ef392c3de6a8fc2febb85bdc763bf25
huangsheng <huangsheng@rrkd.cn>
2015-11-06 11:59:38 +0800

    添加文件

--- app/views.py
+++ app/views.py
@@ -19,4 +19,14 @@ def adduser(nickname,email):
 
 @app.route('/getuser/<nickname>')
 def getuser(nickname):
-    user = User.
+    user = User.query.filter_by(nickname).first()
+    return render_template('user.html', user)
+
+@app.errorhanfler(404)
+def internal_error(error):
+    return render_template('404.html'),404
+
+@app.error_handler(500)
+def internal_error(error):
+    db.session.rollback()
+    return  render_template('500.html'),500
